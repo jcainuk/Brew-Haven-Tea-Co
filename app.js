f2123fb8f0d7ce09f1a95e692877e59418a51620
Jonathan Cain <jcainuk@gmail.com>
2024-03-08 16:03:55 +0000

    saves my variables from the DOM in app.js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const date = (document.getElementById("date").innerHTML =
   new Date().getFullYear());
+
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
